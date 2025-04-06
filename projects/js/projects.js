b364759f678e79a90d477aa182fdcb2b11507e2b
TheDJGamerYT <ghubbell324@gmail.com>
2025-04-06 14:52:25 -0500

    Dvasia + Version 2.4

--- projects/js/projects.js
+++ projects/js/projects.js
@@ -1,7 +1,7 @@
 fetch('projects.json')
   .then(response => response.json())
   .then(data => {
-    const container = document.getElementById('roundabout');
+    const container = document.getElementById('projects');
 
     for (const key in data) {
       const project = data[key];
